0c7385db8af72125c5c1db25d96d0e77bc4482cc
Aki Imai <akimai808@gmail.com>
2018-11-08 11:57:09 -0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
+// const Index = () => <h2>Home</h2>;
+// const About = () => <h2>About</h2>;
+// const Users = () => <h2>Users</h2>;
 
 const AppRouter = () => (
   <Router>
