750c3b931edd54b8a412c380ca10f78374139853
Aki Imai <akimai808@gmail.com>
2018-11-08 11:41:28 -0800

    update

---
+++ client/src/containers/taikoForm.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+class TaikoForm extends React.Component {
+    constructor(props) {
+        super (props)
+
+    }
+
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default TaikoForm; 
