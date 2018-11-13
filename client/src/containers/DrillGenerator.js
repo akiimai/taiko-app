b5fb52446253bf7a738a400cc215e1a3f9bce363
Aki Imai <akimai808@gmail.com>
2018-11-12 16:16:22 -0800

    drill generator service

--- client/src/containers/DrillGenerator.js
+++ client/src/containers/DrillGenerator.js
@@ -1,27 +1,60 @@
 import React from 'react';
+import * as drillsGeneratorServices from '../services/drillsGenerator.services'; 
 
 class DrillGenerator extends React.Component {
     constructor(props) {
-        super(props) 
+        super(props)
 
-    this.onFind = this.onFind.bind(this); 
+        this.state = {
+            select: false
+        }
 
+        this.onFind = this.onFind.bind(this);
     }
 
     onFind() {
-
+        drillsGeneratorServices.readAll()
+            .then(response => {
+                console.log(response)
+            })
+            .catch(console.log)
+        
+        this.setState({
+            select: true
+        })
     }
 
     render() {
+        let select;
+        if (this.state.select) {
+            select =
+                // <section className="bg-primary text-white mb-0" id="list">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-3"></div>
+                            <div className="col-lg-6">
+                                <p>Freelancer is a free bootstrap theme created by Start Bootstrap. 
+                                The download includes the complete source files including HTML, CSS, and JavaScript 
+                                as well as optional LESS stylesheets for easy customization.
+                                </p>
+                            </div>
+                            <div className="col-lg-3"></div>
+
+                        </div>
+                    </div>
+                // </section>
+        }
+
         return (
             <div className="masthead bg-primary text-white text-center">
                 <h4>What do you want to practice?</h4>
-                
+
                 <br /><br />
                 <button class="btn btn-xl btn-outline-light" onClick={this.onFind}>Find Me A Drill</button>
                 <br /><br />
                 <br /><br />
                 <br /><br />
+                {select}
                 <br /><br />
                 <br /><br />
                 <br /><br />
---
+++ client/src/services/drillsGenerator.services.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const readAll = () => {
+    const url = "/api/taiko-drills"
+    const config = {
+        method: "GET"
+    }
+    const promise = axios(url, config) 
+        .then(responseSuccess)
+        .catch(responseError)
+    return promise 
+}
+
+const responseSuccess = response => {
+    console.log(response)
+    return response.data 
+}
+
+const responseError = error => {
+    console.log(error)
+}
+
+export { readAll }
