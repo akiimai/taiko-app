29aee1769776f0a92d198d7a3877302a18fe58aa
Aki Imai <akimai808@gmail.com>
2018-12-05 16:42:13 -0800

    test endpoint

--- node-api/server.js
+++ node-api/server.js
@@ -5,6 +5,10 @@ const routes = require('./app/routes')
 
 app.use(bodyParser.json())
 
+app.use('/node-api/server.js/test', (req, res) => {
+    res.status(200).json({ works: true });
+})
+
 app.use('/api', routes) 
 
 var port = process.env.PORT || 1337
